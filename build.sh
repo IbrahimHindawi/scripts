84eea3e065b9b0900bce36273901427de58c09bd
IbrahimHindawi <ihindawi@gmail.com>
2025-01-19 10:14:31 +0400

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,80 +3,103 @@
 # WARNING:
 # Must be executed from project root directory using `scripts/bbuild.sh`
 
-# TODO (Ibrahim): Implement Debug and Release to compileCommand.
-
 # Setup Script Variables
-projectName=$(basename "$PWD")
-# echo $projectName
+projectname=$(basename "$(pwd)")
+generator="Ninja"
+compilecommand="cmake --build build"
+buildcommand="cmake -B build -G $generator"
+debugger="gdb"
+# debugger="raddbg"
 
-# compileCommand="msbuild build/$projectName.sln -nologo -m -v:m /property:Configuration=Debug /property:VcpkgEnabled=false"
-compileCommand="cmake --build build"
-# compileCommand="cmake --build build --target $projectName"
-
-# Program Begin
 echo "Use -h to display available commands."
 echo
 
-function Help {
-    echo "-b to build solution."
-    echo "-c to compile."
-    echo "-cr to compile and run."
-    echo "-m to compile haikal metaprogram generator."
-    echo "-r to run exe."
-    echo "-x to clean up."
+# Functions for each command
+Help() {
+    echo "-b   to build."
+    echo "-br  to build release."
+    echo "-brd to build release with debug info."
+    echo "-mb  to build haikal meta."
+    echo "-mc  to compile haikal meta."
+    echo "-c   to compile."
+    echo "-cr  to compile and run."
+    echo "-crd to compile and run with debugger."
+    echo "-r   to run exe."
+    echo "-x   to clean up."
 }
 
-function Build {
+Build() {
     mkdir -p build
-    pushd build
-    cmake ..  "-DCMAKE_BUILD_TYPE=Debug" "-DProjectNameParam:STRING=$projectName"
+    $buildcommand "-DCMAKE_BUILD_TYPE=Debug" -DDEBUGRENDER=1
+}
+
+BuildRelease() {
+    mkdir -p build
+    $buildcommand "-DCMAKE_BUILD_TYPE=Release"
+}
+
+BuildReleaseDebug() {
+    mkdir -p build
+    $buildcommand "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+}
+
+Compile() {
+    ./extern/haikal/build/haikal
+    $compilecommand
+}
+
+CompileRun() {
+    ./extern/haikal/build/haikal
+    $compilecommand
+    ./build/$projectname
+}
+
+CompileRunDebugger() {
+    ./extern/haikal/build/haikal
+    $compilecommand
+    $debugger ./build/$projectname
+}
+
+MetaBuild() {
+    echo "Building haikal."
+    pushd extern/haikal
+    echo "$(pwd)"
+    ./scripts/build.sh -x
+    ./scripts/build.sh -b
     popd
-    echo "Building haikal metaprogram generator..."
-    cmake --build build/extern/haikal
 }
 
-function Compile {
-    ./build/extern/haikal/haikal
-    $compileCommand
+MetaCompile() {
+    echo "Compile haikal."
+    pushd extern/haikal
+    echo "$(pwd)"
+    ./scripts/build.sh -c
+    popd
 }
 
-function CompileRun {
-    .build/extern/haikal/haikal
-    $compileCommand
-    build/Debug/$projectName
+Run() {
+    ./build/$projectname
 }
 
-function MetaGen {
-    echo "Building haikal metaprogram generator..."
-    cmake --build build/extern/haikal
-}
-
-function Run {
-    ./build/$projectName
-}
-
-function CleanUp {
+CleanUp() {
     echo "Destroy build folder."
     rm -rf build
 }
 
-# GETOPTS
-while [[ "$1" != "" ]]; do
-    case $1 in
-        -h ) Help
-             ;;
-        -b ) Build
-             ;;
-        -c ) Compile
-             ;;
-        -cr ) CompileRun
-              ;;
-        -m ) MetaGen
-             ;;
-        -r ) Run
-             ;;
-        -x ) CleanUp
-             ;;
+# Main logic for command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h) Help; exit 0 ;;
+        -b) Build; exit 0 ;;
+        -br) BuildRelease; exit 0 ;;
+        -brd) BuildReleaseDebug; exit 0 ;;
+        -mb) MetaBuild; exit 0 ;;
+        -mc) MetaCompile; exit 0 ;;
+        -c) Compile; exit 0 ;;
+        -cr) CompileRun; exit 0 ;;
+        -crd) CompileRunDebugger; exit 0 ;;
+        -r) Run; exit 0 ;;
+        -x) CleanUp; exit 0 ;;
+        *) echo "Unknown option: $1"; Help; exit 1 ;;
     esac
-    shift
 done
